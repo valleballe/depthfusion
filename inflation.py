b43b17d58ca7e3c30a45e59720e3a4ccec3f8134
valleballe <valdemar.muda@gmail.com>
2023-08-07 12:26:18 +0200

    added vertex color

--- inflation.py
+++ inflation.py
@@ -4,22 +4,32 @@ import argparse
 from scipy.sparse import coo_matrix, linalg
 import scipy.ndimage as ndi
 
-def depth2orthomesh(depth, x_step=1, y_step=1, scale=[1.0, 1.0, 1.0], minus_depth=True):
+def depth2orthomesh(depth, color_img_path, x_step=1, y_step=1, scale=[1.0, 1.0, 1.0], minus_depth=True):
+    
+    # Load the color image
+    color_img = cv2.imread(color_img_path)
+    color_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2RGB)
+
     vertices = []
     faces = []
+    colors = []
+
     if len(depth.shape) != 2:
         return None
+
     h, w = depth.shape
     vertex_id = 0
     added_table = {}
+
     for y in range(0, h, y_step):
         for x in range(0, w, x_step):
             added_table[(y, x)] = -1
+            
     max_connect_z_diff = 99999.9
-    # TODO
-    # pixel-wise loop in pure python is toooooooo slow
+
     for y in range(0, h, y_step):
         for x in range(0, w, x_step):
+
             d = depth[y, x]
             if d <= 0.000001:
                 continue
@@ -30,41 +40,37 @@ def depth2orthomesh(depth, x_step=1, y_step=1, scale=[1.0, 1.0, 1.0], minus_dept
 
             added_table[(y, x)] = vertex_id
 
+            # Get color from color image
+            color = color_img[y, x]
+            colors.append(color)
+
             current_index = vertex_id
-            upper_left_index = added_table[((y - y_step), (x - x_step))]
-            upper_index = added_table[((y - y_step), x)]
-            left_index = added_table[(y, (x - x_step))]
+            upper_left_index = added_table[(y - y_step, x - x_step)]
+            upper_index = added_table[(y - y_step, x)]
+            left_index = added_table[(y, x - x_step)]
 
             upper_left_diff = np.abs(depth[y - y_step, x - x_step] - d)
             upper_diff = np.abs(depth[y - y_step, x] - d)
             left_diff = np.abs(depth[y, x - x_step] - d)
 
-            if upper_left_index > 0 and upper_index > 0\
-               and upper_left_diff < max_connect_z_diff\
-               and upper_diff < max_connect_z_diff:
+            if upper_left_index > 0 and upper_index > 0 and upper_left_diff < max_connect_z_diff and upper_diff < max_connect_z_diff:
                 faces.append([upper_left_index, current_index, upper_index])
 
-            if upper_left_index > 0 and left_index > 0\
-                and upper_left_diff < max_connect_z_diff\
-                    and left_diff < max_connect_z_diff:
+            if upper_left_index > 0 and left_index > 0 and upper_left_diff < max_connect_z_diff and left_diff < max_connect_z_diff:
                 faces.append([upper_left_index, left_index, current_index])
 
             vertex_id += 1
-    return vertices, faces
+            
+    return vertices, faces, colors
 
 
-def _make_ply_txt(vertices, faces, color=[], normal=[]):
+def _make_ply_txt(vertices, faces, color=[]):
     header_lines = ["ply", "format ascii 1.0",
                     "element vertex " + str(len(vertices)),
                     "property float x", "property float y", "property float z"]
-    has_normal = len(vertices) == len(normal)
-    has_color = len(vertices) == len(color)
-    if has_normal:
-        header_lines += ["property float nx",
-                         "property float ny", "property float nz"]
-    if has_color:
-        header_lines += ["property uchar red", "property uchar green",
-                         "property uchar blue", "property uchar alpha"]
+    
+    header_lines += ["property uchar red", "property uchar green",
+                     "property uchar blue", "property uchar alpha"]
     # no face
     header_lines += ["element face " + str(len(faces)),
                      "property list uchar int vertex_indices", "end_header"]
@@ -73,26 +79,23 @@ def _make_ply_txt(vertices, faces, color=[], normal=[]):
     data_lines = []
     for i in range(len(vertices)):
         line = [vertices[i][0], vertices[i][1], vertices[i][2]]
-        if has_normal:
-            line += [normal[i][0], normal[i][1], normal[i][2]]
-        if has_color:
-            line += [int(color[i][0]), int(color[i][1]), int(color[i][2]), 255]
+        
+        line += [int(color[i][0]), int(color[i][1]), int(color[i][2]), 255]
         line_txt = " ".join([str(x) for x in line])
         data_lines.append(line_txt)
+
     for f in faces:
         line_txt = " ".join(['3'] + [str(int(x)) for x in f])
         data_lines.append(line_txt)
 
-    data_txt = "\n".join(data_lines)
-
-    ply_txt = header + data_txt
+    ply_txt = header + "\n".join(data_lines)
 
     return ply_txt
 
 
-def writeMeshAsPly(path, vertices, faces):
+def writeMeshAsPly(path, vertices, faces, colors):
     with open(path, 'w') as f:
-        txt = _make_ply_txt(vertices, faces)
+        txt = _make_ply_txt(vertices, faces, colors)
         f.write(txt)
 
 
@@ -311,9 +314,9 @@ def inflationByBaran(mask, use_sparse=True):
     A = coo_matrix((data, (row, col)), shape=(num_param, num_param))
 
     # Solve for 'x' using scipy's sparse linear solver
-    x = linalg.spsolve(A, b)
+    x = linalg.spsolve(A.astype(np.float64), b.astype(np.float64))
 
-     # Fills up the depth array with the computed depths
+    # Fills up the depth array with the computed depths
     z_min = min(x)
     max_depth = 1
 
@@ -336,7 +339,7 @@ def inflationByBaran(mask, use_sparse=True):
 
     return depth
 
-def inflate_mesh(mask_path, output_path):
+def inflate_mesh(mask_path, color_img_path, output_path):
 
     # Read mask of mesh
     mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
@@ -347,23 +350,26 @@ def inflate_mesh(mask_path, output_path):
     depth = inflationByBaran(mask)
 
     # Apply depth to mesh
-    vertices, faces = depth2orthomesh(depth)
+    vertices, faces, colors = depth2orthomesh(depth, color_img_path)
+
 
     #Export mesh
-    writeMeshAsPly(output_path + 'inflated_mesh.ply', vertices, faces)
+    writeMeshAsPly(output_path + 'inflated_mesh.ply', vertices, faces, colors)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Image Processing Script")
 
-    parser.add_argument('--mask_path', type=str, default="data/character.png", help='Path to the masks')
+    parser.add_argument('--mask_path', type=str, default="data/vase_mask.png", help='Path to the masks')
+    parser.add_argument('--color_img_path', type=str, default="data/vase.png", help='Path to the original image')
     parser.add_argument('--output_path', type=str, default="output/", help='Path to output the results')
 
     args = parser.parse_args()
 
     mask_path = args.mask_path
+    color_img_path = args.color_img_path
     output_path = args.output_path
 
 
     # Read mask of mesh
-    inflate_mesh(mask_path, output_path)
+    inflate_mesh(mask_path, color_img_path, output_path)
--- main.py
+++ main.py
@@ -1,16 +1,87 @@
 import os
 import argparse
 
+# UI
+import streamlit as st
+import pyvista as pv
+from stpyvista import stpyvista
+
+# remove background
+import cv2
+import numpy as np
+from rembg import remove
+from PIL import Image	
+
+# own packages
 from inflation import inflate_mesh
 
 def remove_background(img_path, output_path):
-	from rembg import remove
-	from PIL import Image
 
 	image_raw = Image.open(img_path).convert('RGB')
 	output = remove(image_raw, only_mask=True)
 	output.save(output_path)
+
+	# threshold images
+	mask = cv2.imread(output_path, cv2.IMREAD_GRAYSCALE)
+	mask[mask > 100] = 255
+	mask[mask <= 100] = 0
+
+	# make black boundary around image
+	mask[0,:] = 0
+	mask[-1,:] = 0
+	mask[:,0] = 0
+	mask[:,-1] = 0
+
+	# eliminate floating points
+	new_mask = eliminate_floating_regions(mask)
+	cv2.imwrite(output_path, new_mask)
+
 	return output_path
+
+def eliminate_floating_regions(mask):
+    # close everything inside
+	contour, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE);
+
+	# extract alpha channel
+	alpha = mask[:,:]
+
+	# get the biggest contour # returns _, contours, _ if using OpenCV 3
+	biggest_area = -1;
+	biggest = None;
+	for con in contour:
+		area = cv2.contourArea(con);
+		if biggest_area < area:
+			biggest_area = area;
+			biggest = con;
+
+	# smooth contour
+	peri = cv2.arcLength(biggest, True)
+	biggest = cv2.approxPolyDP(biggest, 0.0001 * peri, True)
+
+	# draw white filled contour on black background
+	mask = np.zeros_like(alpha)
+	#cv2.drawContours(contour_img, [big_contour], 0, 255, -1)
+
+
+	# fill in the contour
+	cv2.drawContours(mask, [biggest], -1, 255, -1);
+	return mask
+
+def viz3D(object_path):
+	pv.global_theme.show_scalar_bar = False
+	
+	## Initialize a plotter object
+	plotter = pv.Plotter(window_size=[400,400])
+	
+	#load mesh
+	mesh = pv.read(object_path)
+	#cpos = mesh.plot()
+	## Pass a key to avoid re-rendering at each time something changes in the page
+	stpyvista(plotter, key="pv_cube")
+
+	#plotter = pv.Plotter(off_screen=True)
+	#plotter.add_mesh(mesh)
+
 
 
 if __name__ == '__main__':
@@ -18,24 +89,35 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Main script")
 
 
-	parser.add_argument('--img_path', type=str, default="data/pillow.png", help='Path to the masks')
+	parser.add_argument('--img_path', type=str, default="data/chandelier_magritte.png", help='Path to the masks')
 	parser.add_argument('--output_path', type=str, default="output/", help='Path to output the results')
 
 	args = parser.parse_args()
+
+	# Setup Streamlit APP
+	st.title('DepthFusion')
+	file = st.file_uploader("Please choose a file")
+
+	if file != None:
+		st.image(Image.open(file), caption='Uploaded Images')
 
 	img_path = args.img_path
 	output_path = args.output_path
 
 	# Greyscale with Background removed
 	print("Removing background...")
-	mask = remove_background(img_path, output_path+'mask.png')
+	mask = remove_background(img_path, output_path+'mask.png') ## < --- needs to add black region all across object
+	#mask = output_path+'mask.png'
+	#st.image(Image.open(mask), caption='Mask Image')
 
 	# Generate depth map
 	# depthmap
+	
 
 	# Inflate mesh based on Baran Method
 	print("Inflating mesh...")
-	inflate_mesh(mask, output_path)
+	inflate_mesh(mask, img_path, output_path)
+	#viz3D(output_path + 'inflated_mesh.ply')
 
 	# Mirror inflated mesh
 	print("Mirroring mesh...")
